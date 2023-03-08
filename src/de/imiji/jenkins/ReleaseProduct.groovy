2b958c372c8b93d8083fcb854a3d3d40a15bd342
Anas Saber <anastal2001@yahoo.com>
2023-03-09 00:28:02 +0100

    wip

--- src/de/imiji/jenkins/ReleaseProduct.groovy
+++ src/de/imiji/jenkins/ReleaseProduct.groovy
@@ -22,11 +22,13 @@ class ReleaseProduct {
     }
 
     void tag(String branch, String moduleName) {
-        this.pipeline.sshagent(credentials: ["github"]) {
+        this.pipeline.sshagent(["github"]) {
+            this.pipeline.sh('export GIT_SSH_COMMAND="ssh -oStrictHostKeyChecking=no"')
             this.pipeline.sh("git push --tags")
             this.pipeline.nvm("v" + ReleaseRegistry.NODE_VERSION) {
                 def version = this.pipeline.sh(script: "npm view ${moduleName}@latest version", returnStdout: true).trim()
                 this.pipeline.sh("npm version --no-git-tag-version ${version}-SNAPSHOT")
+                this.pipeline.sh("git add .")
                 this.pipeline.sh("git push --set-upstream origin ${branch}")
             }
         }
