70b153dadf2be5ebefa09b359af1d9d028a1edc4
Anas Saber <anastal2001@yahoo.com>
2023-03-25 21:37:24 +0100

    set

--- src/de/imiji/jenkins/release/ReleaseRegistry.groovy
+++ src/de/imiji/jenkins/release/ReleaseRegistry.groovy
@@ -54,7 +54,7 @@ class ReleaseRegistry {
         this.pipeline.echo("upload to NPM")
         this.pipeline.withCredentials([this.pipeline.string(credentialsId: NPM_CRED_ID, variable: 'NPM_TOKEN')]) {
             this.pipeline.nvm("v" + NODE_VERSION) {
-                this.pipeline.sh("which npm")
+                this.pipeline.sh("npm config set prefix ${this.pipeline.WORKSPACE}")
                 this.pipeline.sh("echo //${REGISTER_URL}/:_authToken=${this.pipeline.NPM_TOKEN} > .npmrc")
                 String version = "1.0.0-SNAPSHOT"
                 try {
